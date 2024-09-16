1adb8b41b5821d6c2c00bbad8c89283de9d02d81
sakshi kesharwani <sakshikesharwani14@gmail.com>
2024-09-16 12:33:20 +0530

    new change

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -12,7 +12,7 @@ function Select({ options, label, className = "", ...props }, ref) {
         className={`px-3 py-2 rounded-lg bg-white text-[#53279b] outline-none focus:bg-gray-50 duration-200 focus:border-[#9e7fd0] border border-gray-200 w-full ${className}`}
       >
         {options?.map((option) => (
-          <option value={option} key={option}>
+          <option value={option} key={option} >
             {option}
           </option>
         ))}
